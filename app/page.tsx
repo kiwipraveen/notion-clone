7eb0de80387da35cb7981bf1670056a375ee3633
Praveenspkj <praveenspkofficial@gmail.com>
2023-11-23 17:56:58 +0530

    setup-completed

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+
+
+export default function Home() {
+  return (
+    <div>
+      <p className='text-red-500 font-bold'>hello notion clone</p>
+      <Button variant="destructive" size="lg">click me</Button>
+      <Button variant="purple" size="sm">click me</Button>
+
+    </div>
+    
+
+  )
+}
